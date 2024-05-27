1421eeadfd2af20af6e9297fdf479c961b05bc17
dako-23 <iordan.megaparts@gmail.com>
2024-05-27 17:35:54 +0300

    .

---
+++ data-types-exercise/08.calculator.js
@@ -0,0 +1,23 @@
+function solve(num1, oper, num2) {
+
+    let result = 0;
+
+    switch (oper) {
+        case '+':
+            result = num1 + num2
+            break;
+        case '-':
+            result = num1 - num2
+            break;
+        case '*':
+            result = num1 * num2
+            break;
+        case '/':
+            result = num1 / num2
+            break;
+    }
+    
+console.log(result.toFixed(2));
+
+}
+solve(5, '+', 10)
