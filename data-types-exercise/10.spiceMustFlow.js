4641292941d3eae81a15d5edca753c10e8955200
dako-23 <iordan.megaparts@gmail.com>
2024-05-28 17:59:19 +0300

    .

---
+++ data-types-exercise/10.spiceMustFlow.js
@@ -0,0 +1,23 @@
+function solve(startingYield) {
+
+    let spiceCount = startingYield;
+    let dayCount = 0;
+
+    while (startingYield >= 100) {
+
+        if (dayCount !== 0) {
+            spiceCount -= 10;
+            let madeSpice = spiceCount - 10;
+        spiceCount += madeSpice
+        }
+        
+    
+        dayCount++
+
+    }
+
+
+
+
+}
+solve(111);
