ad3ff15465cc5aafa580209b442bcca361c19255
dako-23 <iordan.megaparts@gmail.com>
2024-06-05 14:49:35 +0300

    .

---
+++ Arrays/05.maxNumber.js
@@ -0,0 +1,31 @@
+function solve(arr) {
+
+    let topNums = [];
+
+    for (let i = 0; i < arr.length; i++) {
+        let curNum = arr[i];
+        let isTop = true;
+
+        for (let j = i + 1; j < arr.length; j++) {
+            let rightNum = arr[j];
+
+            if (curNum <= rightNum) {
+                isTop = false;
+                break;
+            }
+
+        }
+
+        if (isTop) {
+            topNums.push(curNum);
+        }
+
+
+
+
+    }
+    console.log(topNums.join(" "));
+
+
+}
+solve([1, 4, 3, 2]);
