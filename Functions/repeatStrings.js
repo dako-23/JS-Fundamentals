65ef5e9b22945a47a331edec235cb130a17887bc
dako-23 <iordan.megaparts@gmail.com>
2024-06-09 19:49:56 +0300

    .

---
+++ Functions/repeatStrings.js
@@ -0,0 +1,16 @@
+function repeatStrings(str, count) {
+
+    let result = "";
+
+    for (let i = 0; i < count; i++) {
+
+        result += str;
+
+    }  
+
+
+    return result;
+
+}
+repeatStrings("abc", 3);
+repeatStrings("String", 2);
