7b86e9dccf2332d5830470a6dc7571d3179cca4b
dako-23 <iordan.megaparts@gmail.com>
2024-06-09 18:19:27 +0300

    .

---
+++ Functions/mathPower.js
@@ -0,0 +1,13 @@
+function mathPower(number, power) {
+
+    // const result = number ** power;
+    const result = Math.pow(number, power)
+    console.log(result);
+
+
+
+
+
+}
+mathPower(2, 8);
+mathPower(3, 4);
