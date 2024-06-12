c839db3764414b4b9623bb4ad40b1d1750dfad40
dako-23 <iordan.megaparts@gmail.com>
2024-06-12 20:08:12 +0300

    .

---
+++ Functions/10.factorialDivision.js
@@ -0,0 +1,22 @@
+function factorialDivision(num1, num2) {
+
+
+    function calculateFactorial(num) {
+        let factorial = 1;
+        for (let factor = 2; factor <= num; factor++) {
+            factorial *= factor;
+        }
+        return factorial;
+    }
+
+    const factorial1 = calculateFactorial(num1)
+    const factorial2 = calculateFactorial(num2)
+    const result = factorial1 / factorial2
+    console.log(result.toFixed(2));
+
+
+
+
+
+}
+factorialDivision(5, 2);
