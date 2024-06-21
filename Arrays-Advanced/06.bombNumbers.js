4dfb8568107d111183d6b63ae9b177b2eb274d90
dako-23 <iordan.megaparts@gmail.com>
2024-06-21 15:09:43 +0300

    .

--- Arrays-Advanced/06.bombNumbers.js
+++ Arrays-Advanced/06.bombNumbers.js
@@ -1,6 +1,20 @@
-function bombNumber(arr) {
+function bombNumber(nums, bomb) {
 
+    const [bombNum, power] = bomb
 
+    const bombIdx = nums.indexOf(bombNum)
+
+    // let resultArr = nums.splice(power, bombIdx)
+    // nums.splice(bombIdx, power)
+    // nums.splice(bombIdx, power)
+
+    for (let i = 0; i < nums.length; i++) {
+        const element = nums[i];
+
+        console.log(element);
+    }
+
+    // console.log(nums);
 
 
 }
