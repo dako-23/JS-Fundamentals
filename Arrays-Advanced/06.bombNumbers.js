b18faba42967f1d0526232fd3165af2c531a5a42
dako-23 <iordan.megaparts@gmail.com>
2024-06-21 16:00:47 +0300

    .

--- Arrays-Advanced/06.bombNumbers.js
+++ Arrays-Advanced/06.bombNumbers.js
@@ -8,14 +8,20 @@ function bombNumber(nums, bomb) {
     // nums.splice(bombIdx, power)
     // nums.splice(bombIdx, power)
 
-    for (let i = 0; i < nums.length; i++) {
-        const element = nums[i];
-
-        console.log(element);
+    let newArr1 = nums.slice(0, bombIdx)
+    let newArr2 = nums.slice(bombIdx + 1)
+    if (newArr2.length % 2 === 1) {
+        newArr2.shift();
     }
+    newArr1.splice(-power)
+    newArr2.reverse().splice(power)
 
-    // console.log(nums);
-
+    console.log(newArr1);
+    console.log(newArr2);
 
 }
-bombNumber([1, 2, 2, 4, 2, 2, 2, 9], [4, 2]);
+// bombNumber([1, 2, 2, 4, 2, 2, 2, 9], [4, 2]);
+// bombNumber([1, 4, 4, 2, 8, 9, 1], [9, 3])
+// bombNumber([1, 7, 7, 1, 2, 3], [7, 1])
+bombNumber([1, 1, 2, 1, 1, 1, 2, 1, 1, 1],
+    [2, 1])
