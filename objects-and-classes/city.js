3ed45e8c90ce7f64e135eea0a1567e42ddd6b0a1
dako-23 <iordan.megaparts@gmail.com>
2024-06-30 14:54:12 +0300

    .

---
+++ objects-and-classes/city.js
@@ -0,0 +1,20 @@
+function solve(city) {
+
+    const keys = Object.keys(city);
+    
+    for (const key of keys) {
+        const value = city[key]
+        console.log(`${key} -> ${value}`);
+        
+    }
+
+
+}
+solve
+    ({
+        name: "Sofia",
+        area: 492,
+        population: 1238438,
+        country: "Bulgaria",
+        postCode: "1000"
+    });
