d77c108a1fe89dec7844157113135fc2302eb3a6
dako-23 <iordan.megaparts@gmail.com>
2024-07-03 15:43:50 +0300

    .

--- objects-and-classes/01.employees.js
+++ objects-and-classes/01.employees.js
@@ -1,34 +1,35 @@
 function employees(arr) {
 
-    class information {
-        realnames;
-        personNum;
+    // class information {
+    //     realnames;
+    //     personNum;
 
-        constructor(realnam, personNum) {
-            this.realnam = realnam;
-            this.personNum = personNum;
-        }
+    //     constructor(realnam, personNum) {
+    //         this.realnam = realnam;
+    //         this.personNum = personNum;
+    //     }
 
-        print() {
+    //     print() {
 
-            console.log(`Name: ${realnam} -- Personal Number: ${personNum.length}`);
+    //         console.log(`Name: ${realnam} -- Personal Number: ${personNum}`);
 
+    //     }
 
-        }
+    // }
 
+    for (const employeeNames of arr) {
+
+        personalNum = employeeNames.length
+
+        const employeeObj = {
+            name: employeeNames,
+            number: personalNum
+        };
+        console.log(`Name: ${employeeObj.name} -- Personal Number: ${employeeObj.number}`);
+
+        // const res = new information(realnam, personNum);
+        // res.print()
     }
-
-
-    for (const names of arr) {
-
-        realnam = names;
-        personNum = names.split('');
-
-        const res = new information(realnam, personNum);
-        res.print()
-    }
-
-
 
 }
 employees(['Silas Butler', 'Adnaan Buckley', 'Juan Peterson', 'Brendan Villarreal']);
