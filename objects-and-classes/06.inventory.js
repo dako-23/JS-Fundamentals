b0b8069e9847ced59ed48401d17ee64bb96cd4db
dako-23 <iordan.megaparts@gmail.com>
2024-07-04 17:51:56 +0300

    .

---
+++ objects-and-classes/06.inventory.js
@@ -0,0 +1,13 @@
+function inventory(arr) {
+
+
+
+
+
+
+
+    
+
+
+}
+inventory(['Isacc / 25 / Apple, GravityGun', 'Derek / 12 / BarrelVest, DestructionSword', 'Hes / 1 / Desolator, Sentinel, Antara']);
---
+++ objects-and-classes/07.classVehicle.js
@@ -0,0 +1,9 @@
+class Vehilce {
+
+}
+
+let parts = { engine: 6, power: 100 };
+let vehicle = new Vehicle('a', 'b', parts, 200);
+vehicle.drive(100);
+console.log(vehicle.fuel);
+console.log(vehicle.parts.quality);
