1b2b0128150dbad4229bae334b50e9d55f63325a
dako-23 <iordan.megaparts@gmail.com>
2024-07-05 11:49:57 +0300

    .

--- objects-and-classes/07.classVehicle.js
+++ objects-and-classes/07.classVehicle.js
@@ -1,9 +1,33 @@
-class Vehilce {
+class Vehicle {
+
+    constructor(type, model, parts, fuel) { 
+        this.type = type;
+        this.model = model;
+        this.fuel = fuel
+
+        parts.quality = parts.engine * parts.power
+
+        this.parts = parts;
+
+
+    }
+
+    drive(fuelLoss) {
+
+        this.fuel -= fuelLoss
+
+
+    }
 
 }
 
-let parts = { engine: 6, power: 100 };
-let vehicle = new Vehicle('a', 'b', parts, 200);
-vehicle.drive(100);
+// let parts = { engine: 6, power: 100 };
+// let vehicle = new Vehicle('a', 'b', parts, 200);
+// vehicle.drive(100);
+// console.log(vehicle.fuel);
+// console.log(vehicle.parts.quality);
+
+let parts = {engine: 9, power: 500};
+let vehicle = new Vehicle('l', 'k', parts, 840);
+vehicle.drive(20);
 console.log(vehicle.fuel);
-console.log(vehicle.parts.quality);
