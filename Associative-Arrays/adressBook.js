b7228744fec8fcb419e77bc1f062cd13395880ff
dako-23 <iordan.megaparts@gmail.com>
2024-07-08 13:43:42 +0300

    .

---
+++ Associative-Arrays/adressBook.js
@@ -0,0 +1,31 @@
+function adressBook(arr) {
+
+    const result = {}
+
+    for (const command of arr) {
+
+        const [name, adress] = command.split(":")
+
+        result[name] = adress
+
+
+    }
+
+
+    const entries = Object.entries(result)
+    entries.sort(([nameA, addresA], [nameB, addresB]) => {
+
+        return nameA.localeCompare(nameB);
+
+    })
+
+    for (const [name, address] of entries) {
+
+        console.log(`${name} -> ${address}`);
+        
+    }
+
+
+
+}
+adressBook(['Tim:Doe Crossing', 'Bill:Nelson Place', 'Peter:Carlyle Ave', 'Bill:Ornery Rd']);
