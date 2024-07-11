89b03d253b028ed6ebcad7d770d808bc9baf0150
dako-23 <iordan.megaparts@gmail.com>
2024-07-11 15:52:36 +0300

    .

--- Associative-Arrays/average2.js
+++ Associative-Arrays/average2.js
@@ -34,13 +34,17 @@ function solve(arr) {
     for (const [name, grade] of entries) {
 
         console.log(`${name}: ${grade}`);
-        
+
     }
 
 
 }
-solve([
-    'Lilly 4 6 6 5',
-    'Tim 5 6',
-    'Tammy 2 4 3',
-    'Tim 6 6']);
+// solve([
+//     'Lilly 4 6 6 5',
+//     'Tim 5 6',
+//     'Tammy 2 4 3',
+//     'Tim 6 6']);
+solve(['Steven 3 5 6 4',
+    'George 4 6',
+    'Tammy 2 5 3',
+    'Steven 6 3']);
